0d4d97d71f79c7ee6f41517024f0a5016d9ff376
malgorzatawojnarowska <malgorzatawojnarowska@outlook.com>
2022-04-25 00:01:14 +0200

    Added button

--- app/src/main/java/pl/mawoj/myapp/MainActivity.kt
+++ app/src/main/java/pl/mawoj/myapp/MainActivity.kt
@@ -2,10 +2,19 @@ package pl.mawoj.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val helloTxt = findViewById<TextView>(R.id.textView6)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "You clicked me!"
+            helloTxt.text = "Hello you!"
+        }
     }
 }
